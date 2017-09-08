12127d27fd85a6e93399ac36efbf6f0467ff7812
User E-grant <e-grant@www586.sakura.ne.jp>
2017-09-08 15:24:49 +0900

    config対策

--- config.php
+++ config.php
@@ -1,7 +1,5 @@
 <?php
 ini_set('display_errors', 1);
-define('DSN', 'mysql:host=?????;dbname=?????');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
-
-//this file is fake. please replace your config file.
+define('DSN', 'mysql:host=mysql327.db.sakura.ne.jp;dbname=e-grant_transport_cost');
+define('DB_USERNAME', 'e-grant');
+define('DB_PASSWORD', 'mE7CQKP9');
