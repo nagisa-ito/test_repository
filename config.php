ce4458ff55dd3b5950898a563b3c22941f366968
nagisa-ito <31495086+nagisa-ito@users.noreply.github.com>
2017-09-08 15:23:32 +0900

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-ini_set('display_errors', 1);
-define('DSN', 'mysql:host=?????;dbname=?????');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
-
-//this file is fake. please replace your config file.
