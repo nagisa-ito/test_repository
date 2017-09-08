8f9f6a1895937423c10f57f8f60dd87e33331db5
User E-grant <e-grant@www586.sakura.ne.jp>
2017-09-08 15:31:13 +0900

    add file to .gitignore

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-ini_set('display_errors', 1);
-define('DSN', 'mysql:host=mysql327.db.sakura.ne.jp;dbname=e-grant_transport_cost');
-define('DB_USERNAME', 'e-grant');
-define('DB_PASSWORD', 'mE7CQKP9');
